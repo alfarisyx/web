44b30da9b0fe8ea72cb8135cb18b0c44fc7f1b2a
sava <140477448+alfarisyx@users.noreply.github.com>
2024-12-18 10:39:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
+    echo "<script>alert('Anda belum login'); window.location.href = 'login.php';</script>";
+    exit();
+}
+$username = $_SESSION['username'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            
+        }
+
+    </style>
+</head>
+
+<body>
+   <div class="container">
+    <h1 style="">selamat datang di halaman dashboard <?php echo $username; ?> </h1>
+    <a href="logout.php"> logout </a>
+   </div>
+</body>
+</html>
